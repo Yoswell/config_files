c76ea6de25171417d08148e751c99a10ca12b35b
Yoswell <yoswel2003@gmail.com>
2025-08-10 21:09:33 -0400

    New installing config files

--- setup.sh
+++ setup.sh
@@ -1,109 +1,100 @@
 #!/bin/bash
 
-# ------------------------------
 # Mejorar rendimiento
-# ------------------------------
-sudo tee -a /etc/sysctl.conf >/dev/null <<EOL
-vm.swappiness = 10
-vm.vfs_cache_pressure = 50
-vm.dirty_ratio = 3
-vm.dirty_background_ratio = 5
-vm.watermark_scale_factor = 200
-vm.page-cluster = 3
-vm.min_free_kbytes = 65536
-vm.dirty_writeback_centisecs = 1500
-vm.dirty_expire_centisecs = 3000
-kernel.sched_latency_ns = 6000000
-kernel.sched_min_granularity_ns = 750000
-kernel.sched_wakeup_granularity_ns = 1000000
-EOL
+	sudo echo '
+	vm.swappiness = 10
+	vm.vfs_cache_pressure = 50
+	vm.dirty_ratio = 3
+	vm.dirty_background_ratio = 5
+	vm.watermark_scale_factor = 200
+	vm.page-cluster = 3
+	vm.min_free_kbytes = 65536
+	vm.dirty_writeback_centisecs = 1500
+	vm.dirty_expire_centisecs = 3000
+	kernel.sched_latency_ns = 6000000
+	kernel.sched_min_granularity_ns = 750000
+	kernel.sched_wakeup_granularity_ns = 1000000
+	' > /etc/sysctl.conf 
 
-sudo sysctl -p
+	sudo sysctl -p
 
-# ------------------------------
-# Configuración de wallpapers
-# ------------------------------
-mkdir -p ~/Desktop/wallpapers
-cp ~/tools/config_files/wallpapers/* ~/Desktop/wallpapers
+# Desinstalaciones
+	sudo apt remove vim --purge
+	sudo apt remove nano --purge
 
-sudo mv /usr/share/backgrounds/kali/kali-maze-16x9.jpg /usr/share/backgrounds/kali/kali-maze-16x9-copy.jpg
-sudo mv /usr/share/backgrounds/kali/login.svg /usr/share/backgrounds/kali/login-copy.svg
-
-sudo cp ~/Desktop/wallpapers/2.png /usr/share/backgrounds/kali/kali-maze-16x9.jpg
-sudo cp ~/Desktop/wallpapers/2.png /usr/share/backgrounds/kali/login.svg
-
-# ------------------------------
-# Clonar repositorio de Writeups de CTF
-# ------------------------------
-cd ~/Documents
-[ -d "CTF_writeups" ] &&  rm -rf CTF_writeups
-git clone https://github.com/Yoswell/ctf_writeups.git
-
-# ------------------------------
 # Archivos de configuración
-# ------------------------------
-mkdir -p ~/tools && cd ~/tools
-[ -d "config_files" ] && rm -rf config_files
-git clone https://github.com/Yoswell/config_files.git
+	mkdir -p ~/tools && cd ~/tools
+	[ -d "config_files" ] && rm -rf config_files
+	git clone https://github.com/Yoswell/config_files.git
 
-# ------------------------------
+# Configuración de wallpapers
+	mkdir -p ~/Desktop/wallpapers
+	cp ~/tools/config_files/wallpapers/* ~/Desktop/wallpapers
+
+	sudo mv /usr/share/backgrounds/kali/kali-maze-16x9.jpg /usr/share/backgrounds/kali/kali-maze-16x9-copy.jpg
+	sudo mv /usr/share/backgrounds/kali/login.svg /usr/share/backgrounds/kali/login-copy.svg
+
+	sudo cp ~/Desktop/wallpapers/2.png /usr/share/backgrounds/kali/kali-maze-16x9.jpg
+	sudo cp ~/Desktop/wallpapers/2.png /usr/share/backgrounds/kali/login.svg
+
+# Clonar repositorio de Writeups de CTF
+	cd ~/Documents
+	[ -d "CTF_writeups" ] &&  rm -rf CTF_writeups
+	git clone https://github.com/Yoswell/ctf_writeups.git
+
 # Configurar ZSH
-# ------------------------------
-cp ~/tools/config_files/zshrc ~/.zshrc
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	cd ~/.oh-my-zsh/custom/plugins
+	git clone https://github.com/zsh-users/zsh-autosuggestions.git
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 
-# Instalar plugins para ZSH
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-cd ~/.oh-my-zsh/custom/plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions.git
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+# Powerlevel10k
+	cd ~/.oh-my-zsh
+	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 
-# ------------------------------
-# Configurar tema personalizado para Rofi
-# ------------------------------
-sudo cp -f ~/tools/config_files/rofi/config.rasi /usr/share/rofi/themes/custom.rasi
-
-# ------------------------------
 # Crear enlace simbólico para comandos personalizados
-# ------------------------------
-sudo ln -sf ~/tools/config_files/comandos.txt /usr/bin/comandos
+	cd ~/tools/config_files
+	sudo ln -sf comandos.txt /usr/bin/comandos
+	rm -r comandos.txt
 
-# ------------------------------
 # Estilizar zshrc y kitty para root
-# ------------------------------
-sudo mkdir -p /root/.config/micro/colorschemes
-sudo cp ~/tools/config_files/zshrc /root
-sudo cp ~/tools/config_files/red.micro /root/.config/micro/colorschemes
+	sudo mkdir -p /root/.config/micro/colorschemes
+	sudo cp ~/tools/config_files/zshrc /root
+	sudo cp ~/tools/config_files/red.micro /root/.config/micro/colorschemes
 
-# ------------------------------
 # Configurar apariencia
-# ------------------------------
-cd ~/tools/config_files
-[ -d "mantis" ] && rm -r mantis
-unzip -o mantis.zip
-cp -r mantis/MantiDusk ~/.themes
-cp -r mantis/MantiNight ~/.themes
+	cd ~/tools/config_files
 
-mkdir -p ~/.config/micro/colorschemes
-cp ~/tools/config_files/red.micro ~/.config/micro/colorschemes
-cp ~/tools/config_files/kitty.conf ~/.config/kitty/kitty.conf
-rm mantis.zip
-rm -r mantis
+	cp ~/tools/config_files/zshrc ~/.zshrc
 
-[ -d "~/.icons/Colloid-Purple-Dracula-Dark" ] && rm -rf ~/.icons/Colloid-Purple-Dracula-Dark
-tar -xf icons.tar.gz
-mkdir -p ~/.icons
-mv Colloid-Purple-Dracula-Dark ~/.icons
-rm icons.tar.gz
+	[ -d "mantis" ] && rm -r mantis
+	unzip -o mantis.zip
+	unzip -o cblack.zip
+	cp -r cblack ~/.themes/cblack
+	cp -r mantis/MantiNight ~/.themes/MantiNight
+	rm mantis.zip && cblack.zip
+	rm -r mantis && rm -r cblack
 
-# ------------------------------
+	mkdir -p ~/.config/micro/colorschemes
+	cp red.micro ~/.config/micro/colorschemes
+	cp kitty.conf ~/.config/kitty/kitty.conf
+
+	[ -d "~/.icons/Colloid-Purple-Dracula-Dark" ] && rm -rf ~/.icons/Colloid-Purple-Dracula-Dark
+	tar -xf icons.tar.gz
+	mkdir -p ~/.icons
+	mv Colloid-Purple-Dracula-Dark ~/.icons
+	rm icons.tar.gz
+
+	sudo cp -f rofi/config.rasi /usr/share/rofi/themes/custom.rasi
+	rm -r rofi
+
 # Instalar fuentes JetBrains Mono
-# ------------------------------
-mkdir -p ~/jetbrains
-cd ~/jetbrains
-mv ~/Downloads/JetBrainsMono*.zip . 2>/dev/null
-unzip -o JetBrainsMono*.zip
-sudo mkdir -p /usr/share/fonts/truetype/jetbrains-mono
-sudo mv fonts/ttf/* /usr/share/fonts/truetype/jetbrains-mono
+	mkdir -p ~/jetbrains
+	cd ~/jetbrains
+	mv ~/Downloads/JetBrainsMono*.zip . 2>/dev/null
+	unzip -o JetBrainsMono*.zip
+	sudo mkdir -p /usr/share/fonts/truetype/jetbrains-mono
+	sudo mv fonts/ttf/* /usr/share/fonts/truetype/jetbrains-mono
 
-cd ~
-rm -rf ~/jetbrains
+	cd ~
+	rm -rf ~/jetbrains
--- tools.sh
+++ tools.sh
@@ -1,37 +1,118 @@
 #!/bin/bash
 
-# ------------------------------
 # Windsurf editor de codigo
-# ------------------------------
-sudo apt-get install wget gpg
-wget -qO- "https://windsurf-stable.codeiumdata.com/wVxQEIWkwPUEAGf3/windsurf.gpg" | gpg --dearmor > windsurf-stable.gpg
-sudo install -D -o root -g root -m 644 windsurf-stable.gpg /etc/apt/keyrings/windsurf-stable.gpg
-echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/windsurf-stable.gpg] https://windsurf-stable.codeiumdata.com/wVxQEIWkwPUEAGf3/apt stable main" | sudo tee /etc/apt/sources.list.d/windsurf.list > /dev/null
-rm -f windsurf-stable.gpg
+	sudo apt-get install wget gpg
+	wget -qO- "https://windsurf-stable.codeiumdata.com/wVxQEIWkwPUEAGf3/windsurf.gpg" | gpg --dearmor > windsurf-stable.gpg
+	sudo install -D -o root -g root -m 644 windsurf-stable.gpg /etc/apt/keyrings/windsurf-stable.gpg
+	echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/windsurf-stable.gpg] https://windsurf-stable.codeiumdata.com/wVxQEIWkwPUEAGf3/apt stable main" | sudo tee /etc/apt/sources.list.d/windsurf.list > /dev/null
+	rm -f windsurf-stable.gpg
 
-sudo apt install apt-transport-https
-sudo apt update
+	sudo apt install apt-transport-https
+	sudo apt update
+	sudo apt install windsurf -y
 
-sudo apt install windsurf -y
-
-# ------------------------------
 # Kitty terminal
-# ------------------------------
-sudo apt install kitty -y
+	sudo apt install kitty -y
 
-# ------------------------------
 # Micro editor
-# ------------------------------
-cd ~/tools
-curl -s https://getmic.ro | bash
-sudo cp ./micro /usr/bin
-rm ./micro
+	cd ~/tools
+	curl -s https://getmic.ro | bash
+	sudo cp ./micro /usr/bin
+	rm ./micro
 
-micro -plugin install gotham-colors
-micro -plugin install editorconfig
-micro -plugin install nordcolors
+	micro -plugin install gotham-colors
+	micro -plugin install editorconfig
+	micro -plugin install nordcolors
 
-# ------------------------------
-# lxappearance
-# ------------------------------
-sudo apt install lxappearance -y
+# lxappearance tweaks
+	sudo apt install lxappearance -y
+
+# Rofi launcher
+	sudo apt install rofi -y
+
+# Gdb plugins binary analizer
+	sudo apt install gdb-peda -y
+	curl -qsL 'https://install.pwndbg.re' | sh -s -- -t pwndbg-gdb
+	bash -c "$(curl -fsSL https://gef.blah.cat/sh)"
+
+# Ghidra dissasemble
+	sudo apt install ghidra -y
+
+# Checksec
+	sudo apt install checksec -y 
+
+# Ctf tools pip
+	pip install oletools --break-system-packages
+	pip install stego-lsb --break-system-packages
+	pip install pwntools --break-system-packages
+	pip install ciphey --upgrade --break-system-packages
+	pip3 install stegoveritas --break-system-packages
+	stegoveritas_install_deps
+	pip3 install pypykatz --break-system-packages
+
+# Sonic visualizer
+	mkdir ctftools && cd ctftools
+	curl -O https://code.soundsoftware.ac.uk/attachments/download/2880/SonicVisualiser-5.2.1-x86_64.AppImage
+	mv SonicVisualiser-5.2.1-x86_64.AppImage sonic.AppImage
+	upx sonic.AppImage
+
+# JdGui
+	curl -O https://github.com/java-decompiler/jd-gui/releases/download/v1.6.6/jd-gui-1.6.6.jar
+	mv jd-gui-1.6.6.jar jdGui.jar
+	upx jdGui.jar
+
+# Sqlite browser
+	curl -O https://github.com/sqlitebrowser/sqlitebrowser/releases/download/v3.13.1/DB.Browser.for.SQLite-v3.13.1-x86.64-v2.AppImage
+	mv DB.Browser.for.SQLite-v3.13.1-x86.64-v2.AppImage sqliteBrowser.AppImage
+	upx sqliteBrowser.AppImage
+
+# Cmake
+	sudo apt install cmake -y
+	
+# Pdf cracker
+	git clone https://github.com/MichaelSasser/pdfcrack-ng.git
+	cd pdfcrack-ng
+	mkdir build && cd build
+	cmake ..
+	make
+
+# Binaryninja
+	cd ~/tools/ctftools
+	curl -O https://cdn.binary.ninja/installers/binaryninja_free_linux.zip
+	unzip binaryninja_free_linux.zip
+
+# Stego
+	gem install zsteg
+
+# Stegseek
+	sudo apt install stegseek -y
+
+# Png check
+	sudo apt install pngcheck
+
+# Audio stego
+	sudo apt-get install libboost-all-dev
+	git clone https://github.com/danielcardeenas/AudioStego.git
+	cd AudioStego
+	mkdir build && cd build
+	cmake ..
+	make
+	upx hideme
+	sudo ln -sf ~/tools/ctftools/AudioStego/build/hideme /usr/bin/hideme
+
+# LSB Steganografhy
+	cd ~/tools/ctftool
+	git clone https://github.com/RobinDavid/LSB-Steganography.git
+	cd LSB-Steganography
+	pip install -r requirements.txt --break-system-packages
+	
+# Caido
+	sudo apt install caido -y
+
+# Masscan
+	git clone https://github.com/robertdavidgraham/masscan.git
+	cd masscan
+	make
+
+# Remmina 
+	sudo apt install remmina -y
